d6a2b28ce6a49815ad20f0faee835274db7b21ec
waleed fadl <wwwaleedfadl@gmail.com>
2025-06-08 13:18:58 +0300

    adding corn

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from 'dotenv'
-import { initDB, sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from './routes/transactionsRoute.js'
-
+import job from './config/corn.js'
 /////////////////////////// ABC \\\\\\\\\\\\\\\\\\\\\\
 dotenv.config();
 const app = express()
@@ -13,8 +13,14 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json())
 app.use(rateLimiter)
 
+if (process.env.NODE_ENV === 'production') job.start()
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: "ok" })
+})
+
 ////////////////////////// Initalize \\\\\\\\\\\\\\\\\\\\\\
 initDB()
+
 ////////////////////////// Routes \\\\\\\\\\\\\\\\\\\\\\
 app.use('/api/transactions', transactionsRoute)
 
